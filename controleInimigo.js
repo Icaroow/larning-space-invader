a1d9bb263027411b8e7de2abd4d619a51d13e8d6
Icro03 <icaromilhomemjr01@gmail.com>
2024-09-15 03:29:37 -0300

    space-invader

---
+++ controleInimigo.js
@@ -0,0 +1,33 @@
+export default class EnemyController{
+
+    enemyMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      ];
+
+    enemyRows= [];
+
+    constructor(canvas){
+        this.canvas = canvas;
+        this.createEnemies();
+    }
+    
+    draw(ctx){
+        
+    }
+    createEnemies(){
+        this.enemyMap.array.forEach((row,rowIndex) => {
+            this.enemyRows[rowIndex] = [];
+            row.forEach((enemyNumber, enemyIndex) => {
+                if(enemyNumber > 0){
+                    this.enemyRows[rowIndex].push(new Enemy(enemyIndex* 50, rowIndex * 35, enemyNumber))
+                }
+            })
+        });
+
+    }
+}
